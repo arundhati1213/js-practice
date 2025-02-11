8e7b31b0e31bc2c89adc87e2a01b28c810c4ff43
arundhati1213 <liza.alpha@gmail.com>
2025-02-11 13:05:40 +0530

    variable practice

--- 01_Basic/01_variable.js
+++ 01_Basic/01_variable.js
@@ -1,1 +1,7 @@
+const accountID = 12345; // Can't change
+let accountEmail = "liza.alpha@gmail.com"; //can change
+var accountState = "Delhi"; //can change
+accountCountry = "India"; //can change
+
+console.table(accountID,accountEmail,accountState,accountCountry);
 
