0130cce1e460b34456092710cc1fa1480a1b61ee
arundhati1213 <liza.alpha@gmail.com>
2025-03-05 09:33:52 +0000

    scopes

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -1,13 +1,11 @@
-//var c = 300
+//var c = 300 //globale scope
 let a = 300
 if (true) {
-    let a = 10
+    let a = 10 //scope
     const b = 20
      console.log("INNER: ", a);
     
 }
-
-
 
  console.log(a);
 // console.log(b);
